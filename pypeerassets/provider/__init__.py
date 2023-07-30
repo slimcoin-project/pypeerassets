e173a50c018f2754fdd0ca765cab66d9094c7240
d5000 <5804037+d5000@users.noreply.github.com>
2023-07-30 17:52:59 -0300

    Re-added blockbook provider.

--- pypeerassets/provider/__init__.py
+++ pypeerassets/provider/__init__.py
@@ -2,4 +2,5 @@ from .rpcnode import RpcNode
 from .common import Provider
 from .cryptoid import Cryptoid
 from .explorer import Explorer
+from .blockbook import Blockbook
 from .slm_rpcnode import SlmRpcNode
